8abf5c33eb2e876348f084ded77a25ae74d1c29a
Ivan Lopes <lopesivan.ufrj@gmail.com>
2025-11-20 18:40:24 -0300

    :add: Adiciona o arquivo conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,13 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+
+
+class ExampleRecipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def requirements(self):
+        self.requires("cvplot/1.2.2")
+
+    def layout(self):
+        cmake_layout(self)
